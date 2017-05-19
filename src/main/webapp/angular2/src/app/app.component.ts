ac7cc9075d10fc3ad4884f7e85d8739ac62ba207
Yong Liu <yong.liu2001@gmail.com>
2017-05-19 16:19:04 +1000

    create available room table list

--- src/main/webapp/angular2/src/app/app.component.ts
+++ src/main/webapp/angular2/src/app/app.component.ts
@@ -6,21 +6,27 @@ import { FormControl, FormGroup } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
-  roomSearch: FormGroup;
   public submitted: boolean;
+  roomSearch: FormGroup;
+  rooms: Room[];
 
   ngOnInit(): void {
     this.roomSearch = new FormGroup({
       checkin: new FormControl(''),
       checkout: new FormControl('')
     });
+    this.rooms = ROOMS;
   }
 
   onSubmit({value, valid}: {value: RoomSearch, valid:boolean}) {
     console.log(value);
     console.log(valid);
+  }
+
+  reserveRoom(roomId: string) {
+    console.log("Room id for reservation "+ roomId);
   }
 }
 
@@ -28,3 +34,30 @@ export interface RoomSearch {
   checkin: string;
   checkout: string;
 }
+
+export interface Room {
+  id: string;
+  roomNumber: string;
+  price: string;
+  links: string;
+}
+
+var ROOMS: Room[] = [
+  {
+    "id": "1",
+    "roomNumber": "501",
+    "price": "120",
+    "links": ""
+  },
+  {
+    "id": "2",
+    "roomNumber": "502",
+    "price": "130",
+    "links": ""
+  },
+  {
+    "id": "3",
+    "roomNumber": "503",
+    "price": "150",
+    "links": ""
+  }];
