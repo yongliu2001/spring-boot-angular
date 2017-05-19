c654dd4dae019130bab443be395469c96bcea755
Yong Liu <yong.liu2001@gmail.com>
2017-05-19 13:35:59 +1000

    create angular form component

--- src/main/webapp/angular2/src/app/app.component.ts
+++ src/main/webapp/angular2/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit{
+
+  roomSearch: FormGroup;
+  public submitted: boolean;
+
+  ngOnInit(): void {
+    this.roomSearch = new FormGroup({
+      checkin: new FormControl(''),
+      checkout: new FormControl('')
+    });
+  }
+
+  onSubmit({value, valid}: {value: RoomSearch, valid:boolean}) {
+    console.log(value);
+    console.log(valid);
+  }
+}
+
+export interface RoomSearch {
+  checkin: string;
+  checkout: string;
 }
